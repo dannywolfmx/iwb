17c0e787960a818184c5fb8ef5f754e6c69a930d
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2021-03-02 16:09:07 -0600

    Add UP,DOWN,LEFT,RIGHT move, and write methods

--- ui/worldview.go
+++ ui/worldview.go
@@ -18,6 +18,7 @@ type worldView struct {
 	viewportX, viewportY          position
 	viewportWidth, viewportHeight position
 	world                         world.World
+	needUpdate                    bool
 }
 
 //Sync will sync any single cell in the screen, it is more expensive than use tcell.Screen.Show()
@@ -30,35 +31,59 @@ func (w *worldView) Clear() {
 	w.screen.Clear()
 }
 
+//TODO controle the unrange position. ej: -1
+func (w *worldView) moveViewPortX(newPosition position) {
+	w.viewportX += newPosition
+}
+
+//TODO controle the unrange position. ej: -1
+func (w *worldView) moveViewPortY(newPosition position) {
+	w.viewportY += newPosition
+}
+
 func (w *worldView) Run() {
-	w.printOnScreen(1, 1, "Prueba")
-	w.printOnScreen(1, 2, "Prueba")
 	for {
 		switch ev := w.screen.PollEvent().(type) {
 		case *tcell.EventResize:
 			w.Sync()
 		case *tcell.EventKey:
-			eventKey := ev.Key()
-			if eventKey == tcell.KeyEscape || eventKey == tcell.KeyCtrlC {
+			switch ev.Key() {
+			case tcell.KeyCtrlC:
+				//CTRL + C to exit
 				w.screen.Fini()
 				os.Exit(0)
+			case tcell.KeyUp:
+				//Move the viewPort to the UP
+				w.moveViewPortY(-1)
+			case tcell.KeyDown:
+				//Move the viewPort to the DOWN
+				w.moveViewPortY(1)
+			case tcell.KeyRight:
+				//Move the viewPort to the RIGHT
+				w.moveViewPortX(1)
+			case tcell.KeyLeft:
+				//Move the viewPort to the LEFT
+				w.moveViewPortX(-1)
+			default:
+				//Get Rune to print in the screen
+				w.printOnScreen(ev.Rune())
 			}
 		}
-
+		//Check if the map need to repaint
+		if w.needUpdate {
+			w.screen.Show()
+			w.needUpdate = false
+		}
 	}
 }
 
 //TODO the printer dont works with special characters, just support 1 rune at the time
 //TODO Pass the style by parameter
-func (w *worldView) printOnScreen(x, y position, text string) {
-	for _, c := range text {
-		comb := []rune{}
-		w.screen.SetContent(x, y, c, comb, tcell.StyleDefault)
-		//Move the position to the next rune
-		x++
-	}
-	w.screen.Show()
-
+func (w *worldView) printOnScreen(text rune) {
+	w.screen.SetContent(w.viewportX, w.viewportY, text, nil, tcell.StyleDefault)
+	//Move the position to the next rune
+	w.moveViewPortX(1)
+	w.needUpdate = true
 }
 
 //NewWorldView create a worldView
