9074a77e664dcfa08e0e74f0508a9403638af45c
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2021-03-13 23:35:56 -0600

    Add comment to the documentation Stream.go

--- app/repository/stream/stream.go
+++ app/repository/stream/stream.go
@@ -12,12 +12,14 @@ func load(stream io.Reader, data interface{}) error {
 	decoder := gob.NewDecoder(stream)
 
 	err := decoder.Decode(&data)
-	//we don't need a if becouse the client will check the returned error
+	//we don't need a "if" because the client will check the returned error
 	return err
 }
 
 //Save the "data" to the stream using the gob encoder
 func save(stream io.Writer, data interface{}) error {
+	//Generate a new encoder from the stream
 	encoder := gob.NewEncoder(stream)
+	//Encode the data and return the error (or nil)
 	return encoder.Encode(data)
 }
