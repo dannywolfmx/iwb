6901b5de96be13fd35aeb03db8b1eec3118f30f1
Daniel Menchaca Luna <daniel@localhost>
2021-03-29 15:31:01 -0600

    Add token parseJWT

--- app/domain/service/token.go
+++ app/domain/service/token.go
@@ -1,36 +1,72 @@
 package service
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
 	"encoding/base64"
+	"errors"
 
 	"github.com/dannywolfmx/iwb/app/domain/entity"
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwt"
 )
 
-type TokenService struct{}
+const (
+	userkey = "user"
+)
+
+type TokenService struct {
+	key    *rsa.PrivateKey
+	issuer string
+}
+
+//NewTokenService generate a new token jwt
+func NewTokenService(key *rsa.PrivateKey, issuer string) *TokenService {
+	return &TokenService{
+		key:    key,
+		issuer: issuer,
+	}
+}
 
 func (t *TokenService) GenerateJWT(user *entity.User) (entity.Token, error) {
-	issuer := "github.com/dannywolfmx/iwb/app/domain/entity"
-
 	token := jwt.New()
 
-	token.Set(jwt.IssuerKey, issuer)
-
+	//Set key value to the jwt
+	token.Set(jwt.IssuerKey, t.issuer)
+	token.Set(userkey, user.Name)
 	//Encript
 	//
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return "", err
-	}
 
-	signed, err := jwt.Sign(token, jwa.RS256, key)
+	signed, err := jwt.Sign(token, jwa.RS256, t.key)
 
 	if err != nil {
 		return "", err
 	}
 	//Encode to base64
 	return base64.StdEncoding.EncodeToString(signed), nil
+}
+
+func (t *TokenService) ParseJWT(encodePayload string) (*entity.User, error) {
+	payload, err := base64.StdEncoding.DecodeString(encodePayload)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := jwt.Parse(payload, jwt.WithValidate(true), jwt.WithVerify(jwa.RS256, &t.key.PublicKey))
+
+	if err != nil {
+		return nil, err
+	}
+	userRaw, ok := token.Get(userkey)
+	if !ok {
+		return nil, errors.New("User can't be finded")
+	}
+	user, ok := userRaw.(string)
+
+	if !ok {
+		return nil, errors.New("Can't cast to string the usecase name")
+	}
+
+	return &entity.User{
+		Name: user,
+	}, nil
 }
---
+++ app/domain/service/token_test.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/dannywolfmx/iwb/app/domain/entity"
+)
+
+var key *rsa.PrivateKey
+
+func TestMain(m *testing.M) {
+	var err error
+	key, err = rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("Error on generate rsa key: %s", err)
+	}
+	exitVal := m.Run()
+
+	os.Exit(exitVal)
+}
+
+func TestGenerateJWT(t *testing.T) {
+	userTest := &entity.User{
+		Name: "prueba",
+	}
+
+	service := NewTokenService(key, "https://localhost")
+
+	payload, err := service.GenerateJWT(userTest)
+	if err != nil {
+		t.Fatalf("Error on generate JWT: %s", err)
+	}
+
+	t.Log(payload)
+}
+
+func TestParseJWT(t *testing.T) {
+	userTest := &entity.User{
+		Name: "prueba",
+	}
+
+	service := NewTokenService(key, "https://localhost")
+
+	payload, err := service.GenerateJWT(userTest)
+	if err != nil {
+		t.Fatalf("Error on generate JWT: %s", err)
+	}
+
+	user, err := service.ParseJWT(payload)
+
+	if err != nil {
+		t.Fatalf("Error on parse JWT: %s", err)
+	}
+
+	t.Log(user)
+}
